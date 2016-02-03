7df44bafc05d3f046d8dcb009a12ee30b1201bfd
aseber <aseber@techsouth.cc>
2016-02-02 20:59:17 -0500

    Added ID to item code, and simply put a value of 1 for it in Map

--- src/Item.java
+++ src/Item.java
@@ -1,47 +1,18 @@
-<<<<<<< HEAD
-
-public class Item {
-
-	int id;
-	String name;
-	
-	public Item (int inputID, String inputName) {
-		
-		id = inputID;
-		name = inputName;
-		
-	}
-	
-	@Override
-	public int hashCode() {
-		return id;
-	}
-	
-	public String getName() {
-		
-		return name;
-		
-	}
-	
-	public String toString() {
-		
-		return id + ": " + name;
-		
-	}
-	
-=======
 /**
  * Created by Bradley on 2/1/16.
+ * Modified by Austin on 2/2/16.
  */
 public class Item {
     private String name;
     private String type;
     private String description;
-
-    public Item(String name, String type, String description){
+    private int id;
+    
+    public Item(String name, String type, String description, int id){
         this.name=name;
         this.type=type;
         this.description=description;
+        this.id  = id;
     }
 
     public String getName(){
@@ -58,5 +29,10 @@ public class Item {
         //TODO: Implement how an item should be triggered.
         return true;
     }
->>>>>>> refs/heads/master
+    
+    @Override
+	public int hashCode() {
+		return id;
+	}
+    
 }
