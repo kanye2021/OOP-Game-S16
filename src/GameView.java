4bdefc27593aea3c314b2d2074a840f151e7c889
bmtreuherz <bmtreuherz@ufl.edu>
2016-02-02 22:22:31 -0500

    Started game view

---
+++ src/GameView.java
@@ -0,0 +1,19 @@
+import java.awt.*;
+
+/**
+ * Created by Bradley on 2/2/16.
+ */
+public class GameView extends View {
+    public GameView(){
+        //TODO: Implement constructor
+    }
+
+    @Override
+    void render(Graphics g){
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){}
+
+}
