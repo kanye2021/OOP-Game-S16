9ce01281797bc89bf21bfe895b3c3a8dcf8eddda
dmathew93 <denzel.mathew@gmail.com>
2016-02-07 02:02:12 -0500

    did work on statsCondition

--- src/utilities/conditions/StatCondition.java
+++ src/utilities/conditions/StatCondition.java
@@ -1,46 +1,58 @@
-//package utilities.conditions;
-//
-//import models.Entity;
-//import models.items.TakeableItem;
-//
-///**
-// * Created by denzel on 2/6/16.
-// */
-//public class StatCondition {
-//    public enum EntityComparison{
-//        AT_LEAST() {
-//            protected boolean compare(int count, Entity entity, ItemConditions bag, TakeableItem.Items item) {
-//                if (count <= bag.INVENTORY.getBackpack(entity).getItemCount(item)) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            }
-//        },
-//        EXACTLY() {
-//            protected boolean compare(int count, Entity entity, ItemConditions bag, TakeableItem.Items item) {
-//                if (count == bag.INVENTORY.getBackpack(entity).getItemCount(item)) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            }
-//        },
-//        AT_MOST() {
-//            protected boolean compare(int count, Entity entity, ItemConditions bag, TakeableItem.Items item) {
-//                if (count >= bag.INVENTORY.getBackpack(entity).getItemCount(item)) {
-//                    return true;
-//                } else {
-//                    return false;
-//                }
-//            }
-//        };
-//    }
-//    public enum StatConditions{
-//        STRENGTH(){},
-//        AGILITY(){},
-//        INTELLECT(){};
-//
-//        //Function to grab the stats of the Entity
-//    }
-//}
+package utilities.conditions;
+
+import models.Entity;
+import models.Stats;
+import models.items.TakeableItem;
+
+/**
+ * Created by denzel on 2/6/16.
+ */
+public class StatCondition extends Condition {
+    public enum StatsComparison{
+        AT_LEAST(){
+            protected boolean isValid(int entityStat, int requiredStat) {
+                return entityStat >= requiredStat;
+            }
+        },
+        EXACTLY(){
+            protected boolean isValid(int entityStat, int requiredStat) {
+                return entityStat == requiredStat;
+            }
+        },
+        AT_MOST(){
+            protected boolean isValid(int entityStat, int requiredStat) {
+                return entityStat <= requiredStat;
+            }
+        };
+
+        protected abstract boolean isValid(int entityStat, int requiredStat);
+
+        //Returns the ordinal for the Enum
+        public int getID() {
+            return ordinal();
+        }
+    }
+
+
+    //Properties of StatCondition
+    private StatsComparison comparison;
+    private Stats.Type stats;
+    private int requiredAmount;
+
+    //Constructor
+    public StatCondition(Entity entity, StatsComparison comparison, int requiredAmount, Stats.Type stats){
+        this.entity = entity;
+        this.comparison = comparison;
+        this.stats = stats;
+        this.requiredAmount = requiredAmount;
+    }
+
+    @Override
+    protected boolean checkCondition() {
+       int entityStat = stats.get(entity);
+       int requiredStat = this.requiredAmount;
+        
+       return (comparison.isValid(entityStat, requiredStat));
+
+    }
+}
