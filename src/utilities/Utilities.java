e6ec0927629b38725c85ff43bfaa8e11ea12931a
aseber <aseber@techsouth.cc>
2016-02-12 11:27:39 -0500

    Added some comments to Utilities, fixed a method in LoadGameView to use said method from Utilities

--- src/utilities/Utilities.java
+++ src/utilities/Utilities.java
@@ -3,7 +3,15 @@ package utilities;
 import javax.swing.*;
 
 /**
- * Created by aseber on 2/9/16.
+ * Created by aseber
+ * on 2/9/16.
+ */
+
+/*
+ * The purpose of this class is to hold default functions that are used throughout the program.
+ * I've included our file system dependent path setter, and a function for getting pictures.
+ * The purpose for including a picture getter is so that if the image does not exist,
+ * the program has a way to complain and also display what is wrong with a red "X"
  */
 public class Utilities {
 
